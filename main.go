78953c5e984de779604a15f8f07c841120599a0d
Sergey Kozachenko <sergiy.kozachenko@gmail.com>
2015-07-25 02:18:13 +0300

    add insta integration

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
   "os"
   "github.com/go-martini/martini"
+  "github.com/yanatan16/golang-instagram/instagram"
+  "encoding/json"
+  //"fmt"
 )
 
 func main() {
@@ -15,6 +18,42 @@ func main() {
 
   m.Get("/hello", func() string {
     return "world!"
+  })
+
+  m.Get("/checkInsta", func() string {
+    instaClienKey := "ec769078ea8a4872b2dbf0c403168939";
+    // unauthorized api client pass only clientId
+    // for auth need pass second param - token
+    insta := instagram.New(instaClienKey, "")
+
+    response, err := insta.GetMediaPopular(nil);
+    if err != nil {
+  		panic(err)
+  	}
+  	//fmt.Println("Successfully created instagram.Api without user credentials")
+    res1B, _ := json.Marshal(response)
+    return string(res1B)
+      //return json.Marshal(response);
+  })
+
+  m.Get("/panic", func() string {
+    panic("Hey I am in PANIC! NOW!")
+  })
+
+  m.Get("/checkMyInsta", func() string {
+    instaClienKey := "ec769078ea8a4872b2dbf0c403168939";
+    // unauthorized api client pass only clientId
+    // for auth need pass second param - token
+    insta := instagram.New(instaClienKey, "")
+
+    response, err := insta.GetUserRecentMedia("comeseephilippines", nil);
+    if err != nil {
+  		panic(err)
+  	}
+  	//fmt.Println("Successfully created instagram.Api without user credentials")
+    res1B, _ := json.Marshal(response)
+    return string(res1B)
+      //return json.Marshal(response);
   })
   /* Martini's Run function looks for
      the PORT and HOST environment variables
---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+var api = window.api || {};
+
+api.hello = function(){
+  window.location.href = '/hello';
+};
+
+api.checkInsta = function(){
+  window.location.href = '/checkInsta';
+};
