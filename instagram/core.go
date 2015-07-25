0b5e4464428f0a2edacd62ce2f0cf5919c3a898c
Sergey Kozachenko <sergiy.kozachenko@gmail.com>
2015-07-25 23:45:21 +0300

    use local instagram

--- instagram/core.go
+++ instagram/core.go
@@ -40,6 +40,8 @@ func buildGetRequest(urlStr string, params url.Values) (*http.Request, error) {
 	}
 
 	// If we are getting, then we can't merge query params
+	fmt.Println("params:",params)
+	fmt.Println("uRawQuery:",u.RawQuery)
 	if params != nil {
 		if u.RawQuery != "" {
 			return nil, fmt.Errorf("Cannot merge query params in urlStr and params")
