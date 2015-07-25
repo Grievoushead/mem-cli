41fba8f8fd046d2723045e28091df4bc34bf48cb
Sergey Kozachenko <sergiy.kozachenko@gmail.com>
2015-07-25 08:47:34 +0300

    use insta package from github not local

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
   "os"
   "github.com/go-martini/martini"
-  //"github.com/yanatan16/golang-instagram/instagram"
-  "./instagram"
+  "github.com/yanatan16/golang-instagram/instagram"
+  //"./instagram"
   "encoding/json"
   //"fmt"
   "net/url"
